7318b8c0e5dcb4a7e780e7a4ce69fea9626d64df
Alan Jaouen <alan.jaouen@epita.fr>
2016-12-14 00:22:40 +0100

    [add] wind structure

---
+++ app/wind.ts
@@ -0,0 +1,19 @@
+export class Wind {
+  duration:number;
+  latitude:number;
+  longitude:number;
+  recipients:Array<number>;
+  image:string;
+
+  constructor()
+  {
+    this.longitude = 2.3624606;
+    this.latitide = 48.8150901;
+    recipients = Array<number>;
+      
+    //   this.recipients
+  }
+
+}
+
+
