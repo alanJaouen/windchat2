101524aef536d3c3441880a9d5041873465761b2
Alan Jaouen <alan.jaouen@epita.fr>
2016-12-18 07:53:36 +0100

    [feature] icon in navbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -14,15 +14,33 @@ import { HeroService } from './hero.service';
     <img id="topLogo" src="../res/windchat-logo-white-banner.png">
   </header>
     <nav *ngIf="menu">
-      <a routerLink="/dashboard" (click)="menu = !menu;" routerLinkActive="active">Home</a><br/>
-      <a *ngIf="!this.heroService.getHeros().isLog" routerLink="/login" (click)="menu = !menu;" routerLinkActive="active">Login</a><br/>
-      <a *ngIf="!this.heroService.getHeros().isLog" routerLink="/register" (click)="menu = !menu;" routerLinkActive="active">Register</a><br/>
-      <a *ngIf="this.heroService.getHeros().isLog" routerLink="/detail" (click)="menu = !menu;" routerLinkActive="active">Account</a><br/>
-      <a *ngIf="this.heroService.getHeros().isLog" routerLink="/pass" (click)="menu = !menu;" routerLinkActive="active">Change password</a><br/>
-      <a *ngIf="this.heroService.getHeros().isLog" routerLink="/friends" (click)="menu = !menu;" routerLinkActive="active">Friends</a><br/>
-      <a *ngIf="this.heroService.getHeros().isLog" routerLink="/winds" (click)="menu = !menu;" routerLinkActive="active">Send winds</a><br/>
-      <a *ngIf="this.heroService.getHeros().isLog" routerLink="/display" (click)="menu = !menu;" routerLinkActive="active">Display winds</a><br/><br/>
-      <a *ngIf="this.heroService.getHeros().isLog" routerLink="/dashboard" (click)="this.load(); this.hero.isLog=false" routerLinkActive="active">Disconnect</a>
+      <a routerLink="/dashboard" (click)="menu = !menu;" routerLinkActive="active">
+         <img src="../res/home.png"><span>Home</span>
+      </a><br/>
+      <a *ngIf="!this.heroService.getHeros().isLog" routerLink="/login" (click)="menu = !menu;" routerLinkActive="active">
+        <img src="../res/login.png"><span>Login</span>
+      </a><br/>
+      <a *ngIf="!this.heroService.getHeros().isLog" routerLink="/register" (click)="menu = !menu;" routerLinkActive="active">
+        <img src="../res/register.png"><span>Register</span>
+      </a><br/>
+      <a *ngIf="this.heroService.getHeros().isLog" routerLink="/detail" (click)="menu = !menu;" routerLinkActive="active">
+        <img src="../res/account.png"><span>Account</span>
+      </a><br/>
+      <a *ngIf="this.heroService.getHeros().isLog" routerLink="/pass" (click)="menu = !menu;" routerLinkActive="active">
+        <img src="../res/key.png"><span>Change password</span>
+      </a><br/>
+      <a *ngIf="this.heroService.getHeros().isLog" routerLink="/friends" (click)="menu = !menu;" routerLinkActive="active">
+        <img src="../res/friend.png"><span>Friends</span>
+      </a><br/>
+      <a *ngIf="this.heroService.getHeros().isLog" routerLink="/winds" (click)="menu = !menu;" routerLinkActive="active">
+        <img src="../res/send.png"><span>Send winds</span>
+      </a><br/>
+      <a *ngIf="this.heroService.getHeros().isLog" routerLink="/display" (click)="menu = !menu;" routerLinkActive="active">
+        <img src="../res/wind.png"><span>Display winds</span>
+      </a><br/><br/>
+      <a *ngIf="this.heroService.getHeros().isLog" routerLink="/dashboard" (click)="this.load(); this.hero.isLog=false" routerLinkActive="active">
+       <img src="../res/out.png"><span>Disconnect</span>
+      </a>
     </nav>
     <router-outlet></router-outlet>
   `,
